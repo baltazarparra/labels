6a1922580df1cf822ac04da0cc2a51f96a3fe829
Baltazar Parra <btparra@esapiens.com.br>
2018-12-03 06:01:43 -0200

    Add static markup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <header>
+          <h1>Labels</h1>
+          <a href="/">Home</a>
+        </header>
+        <main>
+          <label>
+            github.com/
+            <input placeholder="username..." />
+          </label>
+          <button>Search repos</button>
+        </main>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
