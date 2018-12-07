b1003a8f81aab4c95e8c944a0755df6329d4063a
Baltazar Parra <btparra@esapiens.com.br>
2018-12-07 06:37:28 -0200

    Updates

---
+++ precache-manifest.734252bf105dccd3bfce5a84007451e5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f44a25bc19a9e37d036",
+    "url": "/labels/static/js/runtime~main.9f44a25b.js"
+  },
+  {
+    "revision": "7cfcf17c11f4439c65e7",
+    "url": "/labels/static/js/main.7cfcf17c.chunk.js"
+  },
+  {
+    "revision": "f5dc116eb13140ad83df",
+    "url": "/labels/static/js/1.f5dc116e.chunk.js"
+  },
+  {
+    "revision": "1322cc1779fd2da7224bfd01c9f9a8be",
+    "url": "/labels/index.html"
+  }
+];
