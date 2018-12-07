d0556ed7e1e022985851d3aade6cb5ef7f79d24e
Baltazar Parra <btparra@esapiens.com.br>
2018-12-07 20:11:17 -0200

    Updates

--- precache-manifest.6119e5d0e8f4aec5860cd36c2a98a001.js
+++ precache-manifest.6119e5d0e8f4aec5860cd36c2a98a001.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/labels/static/js/runtime~main.9f44a25b.js"
   },
   {
-    "revision": "cee806934e57db835d11",
-    "url": "/labels/static/js/main.cee80693.chunk.js"
+    "revision": "d44db7dd8d1d7b4b00e5",
+    "url": "/labels/static/js/main.d44db7dd.chunk.js"
   },
   {
     "revision": "f5dc116eb13140ad83df",
     "url": "/labels/static/js/1.f5dc116e.chunk.js"
   },
   {
-    "revision": "539c1bd6fc921fc3172d2f6642027b6b",
+    "revision": "882d9ba3d94e76636358a425e96099ab",
     "url": "/labels/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/labels/precache-manifest.8ce82425e54436a1d8de77be92c57e43.js"
+  "/labels/precache-manifest.6119e5d0e8f4aec5860cd36c2a98a001.js"
 );
 
 workbox.clientsClaim();
